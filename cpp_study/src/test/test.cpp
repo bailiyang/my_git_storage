775ec1fbe5edbcc9b8b53a564a42a9e2229fb5ce
bailiyang <bailiyang@meizu.com>
2017-03-22 18:01:33 +0800

    cpp学习代码提交

--- cpp_study/src/test/test.cpp
+++ cpp_study/src/test/test.cpp
@@ -3,11 +3,39 @@
 
 using namespace std;
 
+class line
+{
+    public:
+        static int objectCount;
+
+        static int getCount()
+        {
+            return objectCount;
+        }
+
+        static void setCount(int count)
+        {
+            objectCount = count;
+        }
+
+        line()
+        {
+            this->objectCount ++;
+            std::cout << "init line success objectCount = " <<this->objectCount << std::endl;
+        }
+
+    private:
+        static int lenth;
+};
+
+
+int line::objectCount = 0;
 
 int main(int argc, char *argv[])
 {
-    time_t time_now = time(0);
-
-    struct tm *time_tm = localtime(&time_now);
-    std::clog << time_tm->tm_hour << std::endl;
+    line temp;
+    line *ptr;
+    ptr = &temp;
+    line::setCount(100);
+    std::cout << line::getCount() << std::endl;
 }
