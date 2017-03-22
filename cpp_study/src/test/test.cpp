8879b2fda3614c58464036c8f80f1e9e8b7df557
root <bly1991@126.com>
2017-03-22 09:06:18 -0700

    cpp学习-类的继承不分

--- cpp_study/src/test/test.cpp
+++ cpp_study/src/test/test.cpp
@@ -3,39 +3,47 @@
 
 using namespace std;
 
-class line
+class father_class
 {
     public:
-        static int objectCount;
-
-        static int getCount()
-        {
-            return objectCount;
+        int getLenth(){
+            return this->lenth;
         }
 
-        static void setCount(int count)
-        {
-            objectCount = count;
+        void setLenth(int len){
+            this->lenth = len;
         }
-
-        line()
-        {
-            this->objectCount ++;
-            std::cout << "init line success objectCount = " <<this->objectCount << std::endl;
-        }
-
     private:
-        static int lenth;
+        int lenth;
 };
 
 
-int line::objectCount = 0;
+class child_class: public father_class{
+    //继承父类，可以使用父类方法
+    public:
+        void swap(int &a, int &b){
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        int operator + (child_class &f){
+            child_class temp;
+            temp.setLenth(this->getLenth() + f.getLenth());
+            return temp.getLenth();
+        }
+
+};
 
 int main(int argc, char *argv[])
 {
-    line temp;
-    line *ptr;
-    ptr = &temp;
-    line::setCount(100);
-    std::cout << line::getCount() << std::endl;
+    child_class f;
+    f.setLenth(10);
+    printf("lenth is %d\n", f + f);
+    int a = 10;
+    int b = 20;
+    f.swap(a, b);
+    printf("swap result is a = %d, b = %d\n", a, b);
 }
+
