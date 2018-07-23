dc88512e1625328d44af15c335e81f2f9dc861ee
bailiyang <bly1991@126.com>
2018-07-23 18:32:26 +0800

    修改自动化脚本

--- vim/vim_env_builder.sh
+++ vim/vim_env_builder.sh
@@ -4,18 +4,17 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 #c++编译环境
-apt install gcc g++ make cmake open-ssl
-apt install golang-go
-apt install gcc-7 g++-7
-apt install gcc-5 g++-5
-apt install gcc-4.8 g++-4.8
+apt install -y gcc g++ make cmake open-ssl
+apt install -y gcc-7 g++-7
+apt install -y gcc-5 g++-5
+apt install -y gcc-4.8 g++-4.8
 #设置gcc版本切换
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 25
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 100
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 
 #go相关环境
-apt remove golang-go
+apt -y remove golang-go
 wget https://dl.google.com/go/go1.10.3.linux-amd64.tar.gzi /opt
 tar -C /usr/local -xzf /opt/go1.10.3.linux-amd64.tar.gz
 #go env
@@ -25,15 +24,15 @@ echo "export GOPATH=/build/go_path" >> ~/.bashrc
 echo "export PATH=$PATH:$GOPATH:/usr/local/go/bin" >> ~/.bashrc
 
 #python编译环境
-apt install python3 python2.7
-apt install python3-dev python2.7-dev
+apt install -y python3 python2.7
+apt install -y python3-dev python2.7-dev
 #设置python版本切换
 update-alternatives --install /usr/bin/python python /usr/bin/python2.7 50
 update-alternatives --install /usr/bin/python python /usr/bin/python3 100
 
 #vim相关环境
-apt install vim
-apt install ack-grep
+apt install -y vim
+apt install -y ack-grep
 #amix/vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 #Vundle
